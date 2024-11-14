bc9a8cf7e12c7d491934b417426cae3bfa0a3219
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2024-11-14 20:52:18 +0700

    add moving action in all direction

--- interactive.py
+++ interactive.py
@@ -1,9 +1,34 @@
 import turtle
 
-def move_forward():
-     turtle.forward(100)
+SPEED = 10
 
-turtle.onkey(move_forward, "Up")
+def move_up():
+    if turtle.heading != 90:
+        turtle.setheading(90)
+    turtle.forward(SPEED)
+
+def move_down():
+    if turtle.heading != 270:
+        turtle.setheading(270)
+    turtle.forward(SPEED)
+
+def move_left():
+    if turtle.heading != 180:
+        turtle.setheading(180)
+    turtle.forward(SPEED)
+
+def move_right():
+    if turtle.heading != 0:
+        turtle.setheading(0)
+    turtle.forward(SPEED)
+
+turtle.speed(0)
+
+turtle.onkey(move_up, "Up")
+turtle.onkey(move_down, "Down")
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
+
 
 turtle.listen()
 turtle.done()
